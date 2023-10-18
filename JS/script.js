5af51d2619ab777818911bee7126b78f4553c30d
Chinonso Ezeokoli <138489844+Nonsookoli007@users.noreply.github.com>
2023-10-18 11:59:23 +0100

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,19 @@
+// $(document).ready(function (){
+
+//     $('.fa-hamburger').click(function (){
+//         $(this).toggleClass('fa-hamburger');
+//         $('.navbar').toggleClass('.navbar-toggle');
+//     });
+//     $('.navbar .unlisted li a').click(function () {
+//         $('.fa-hamburger').removeClass(fa - times);
+//     $('.navbar').removeClass('.navbar-togle');
+// });
+
+//     });
+// $(window).on('scroll load',function(){
+//     if($(window).scrolltop() > 10){
+//         $('header').addClass('header-active');
+//     }else{
+//         $('#header').removeClass('heaser-active');
+//     }
+// });
